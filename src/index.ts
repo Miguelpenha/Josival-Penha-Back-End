a371d6810119a8df37ffee2334472ea6b3c84f90
Miguel Penha <miguel.penha1@gmail.com>
2023-01-15 09:09:32 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,6 @@ console.log(success('>> Cors being used'))
 app.use(helmet())
 console.log(success('>> Helmet being used'))
 
-app.use('/public', express.static(path.resolve(__dirname, '../public')))
 app.use('/', routes)
 
 app.listen(process.env.PORT, () => console.log(success('>> Server is running')))
