64869c648452f44dd8cc4b3abf30cb9ee7f080fb
Miguel Penha <miguel.penha1@gmail.com>
2023-01-28 04:48:51 -0300

    Otimizando router export

--- src/services/generateSpreadsheetService/exportSpreadsheet.ts
+++ src/services/generateSpreadsheetService/exportSpreadsheet.ts
@@ -1,24 +1,15 @@
 import { Workbook } from 'exceljs'
 import { Response } from 'express'
-import path from 'path'
-import fs from 'fs'
 
 async function exportSpreadsheet(name: string, spreadsheet: Workbook, res: Response) {
-    const pathSpreadsheet = path.resolve(__dirname, '..', '..', '..', 'public', 'spreadsheets', `${name}.xlsx`)
-
-    await spreadsheet.xlsx.writeFile(pathSpreadsheet)
-
-    const statSpreadsheet = fs.statSync(pathSpreadsheet)
-
     res.setHeader('Content-Description', 'File Transfer')
     res.setHeader('Content-Disposition', `attachment; filename=${name}.xlsx`)
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    res.setHeader('Content-Length', statSpreadsheet.size)
     res.setHeader('Content-Transfer-Encoding', 'binary')
     res.setHeader('Cache-Control', 'must-revalidate')
     res.setHeader('Pragma', 'public')
-    
-    res.download(pathSpreadsheet, `${name}.xlsx`, () => fs.unlinkSync(pathSpreadsheet))
+
+    await spreadsheet.xlsx.write(res)
 }
 
 export default exportSpreadsheet
