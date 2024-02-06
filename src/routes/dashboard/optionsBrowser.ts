70dd5bf3dffe535e611ee2a482c0897d546be12e
Miguel Penha <miguel.penha1@gmail.com>
2024-02-05 21:17:50 -0300

    Teste

--- src/routes/dashboard/optionsBrowser.ts
+++ src/routes/dashboard/optionsBrowser.ts
@@ -1,6 +1,8 @@
 import { PuppeteerLaunchOptions } from 'puppeteer'
 
 const optionsBrowser: PuppeteerLaunchOptions = {
+    headless: true,
+    args: ['--no-sandbox'],
     defaultViewport: { width: 1920, height: 1080 }
 }
 
