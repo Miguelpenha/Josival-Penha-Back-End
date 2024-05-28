2426c9ac0250ef5a764d73f15960fe4f0210a81a
Miguel Penha <miguel.penha1@gmail.com>
2024-05-28 00:14:12 -0300

    Adicionando rotas de companies

--- src/routes/nyxel/index.ts
+++ src/routes/nyxel/index.ts
@@ -1,8 +1,28 @@
 import express from 'express'
 import videoRouter from './video'
+import companies from './video/companies'
+import ICompany from './video/company'
 
 const routerNyxel = express.Router()
 
 routerNyxel.use('/video', videoRouter)
+
+routerNyxel.get('/companies', async (req, res) => {
+    res.json(companies)
+})
+
+routerNyxel.get('/companies/:companyFolderURL', async (req, res) => {
+    const { companyFolderURL } = req.params
+
+    let company = null as any as ICompany
+
+    companies.map(companyMap => {
+        if (companyMap.folderURL === companyFolderURL) {
+            company = companyMap
+        }
+    })
+
+    res.json(company)
+})
 
 export default routerNyxel
