5d6bce69b6f93ff5a313be300d2e8c0b86d612b0
Miguel Penha <miguel.penha1@gmail.com>
2024-08-05 19:15:17 -0300

    Update pathCache.ts

--- src/routes/kommo/BoldAPI/cache/pathCache.ts
+++ src/routes/kommo/BoldAPI/cache/pathCache.ts
@@ -1,5 +1,7 @@
 import path from 'path'
 
-const pathCache = path.resolve(__dirname, 'BoldAPICache.json')
+const pathCache = path.resolve(__dirname, '..', '..', '..', '..', '..', 'src', 'routes', 'kommo', 'BoldAPI', 'cache', 'BoldAPICache.json')
+
+console.log(pathCache)
 
 export default pathCache
