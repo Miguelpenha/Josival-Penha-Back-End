c1db63d98062cfda0c6e3c783e8314735fca5e2b
Miguel Penha <miguel.penha1@gmail.com>
2024-10-28 16:53:03 -0300

    Update index.ts

--- src/routes/kommo/index.ts
+++ src/routes/kommo/index.ts
@@ -12,7 +12,9 @@ kommoRouter.post('/:companyID', async (req: Request<{ companyID: string }, {}, I
 	const { leads } = req.body
 	const lead = leads.status[0]
 
+	
 	const company = companies.filter(company => companyID.toUpperCase() === company.id)[0]
+	console.log('test', company)
 	const statusID = Object.values(company.statusIDs).map(ID => lead.status_id == ID && ID).filter(value => value)[0]
 
 	res.sendStatus(200)
